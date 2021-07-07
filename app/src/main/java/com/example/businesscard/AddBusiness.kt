9854ca80a73f00b877b5c153e739c83c7d54d68e
Diego_braz <you@example.com>
2021-07-06 23:24:23 -0300

    Create new card layout

---
+++ app/src/main/java/com/example/businesscard/AddBusiness.kt
@@ -0,0 +1,11 @@
+package com.example.businesscard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBusiness : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_business)
+    }
+}
