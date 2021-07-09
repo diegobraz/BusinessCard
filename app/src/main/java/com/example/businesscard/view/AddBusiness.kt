35f16a7990940e390cb53a3abcf1b25ba3b0d80d
Diego_braz <you@example.com>
2021-07-08 23:49:23 -0300

    implements button confirm && save data in dataBase

--- app/src/main/java/com/example/businesscard/view/AddBusiness.kt
+++ app/src/main/java/com/example/businesscard/view/AddBusiness.kt
@@ -2,13 +2,24 @@ package com.example.businesscard.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.example.businesscard.App
 import com.example.businesscard.R
 import com.example.businesscard.databinding.ActivityAddBusinessBinding
 import com.example.businesscard.databinding.ActivityMainBinding
+import com.example.businesscard.domain.BusinessCard
+import com.example.businesscard.view.adapter.BusinessCardAdapter
+import com.example.businesscard.view.viewModel.MainViewModel
+import com.example.businesscard.view.viewModel.MainViewModelFactory
 
 class AddBusiness : AppCompatActivity() {
 
     private val biding by lazy { ActivityAddBusinessBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels{
+        MainViewModelFactory((application as App).repositorie)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +31,17 @@ class AddBusiness : AppCompatActivity() {
         biding.closeBtn.setOnClickListener {
             finish()
         }
-
+        biding.btnConfirmar.setOnClickListener {
+            val businessCard = BusinessCard(
+                nome = biding.txtName?.text.toString(),
+                telefone = biding.txtPhone?.text.toString(),
+                email= biding.txtEmail?.text.toString(),
+                empresa= biding.txtEnterprise.text.toString(),
+                fundoPersonalizado= biding.txtColor?.text.toString()
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, "Cartão adicionado com sucesso", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
 }
